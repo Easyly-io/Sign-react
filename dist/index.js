8da84abd5e4e41ad26371df2decb50f3bcbc8cbf
Niv <niv@apo.co.il>
2023-05-18 17:44:21 -0400

    no message

--- dist/index.js
+++ dist/index.js
@@ -13,10 +13,9 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-var moment = require("moment"); // const endURL = "http://localhost:3002";
+var moment = require("moment");
 
-
-var endURL = "https://sign.easyly.com";
+var endURL = "http://localhost:3002"; // const endURL = "https://sign.easyly.com";
 
 var ContractEditor = function ContractEditor(_ref) {
   var publishKey = _ref.publishKey,
